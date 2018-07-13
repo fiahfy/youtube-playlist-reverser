11f43a24d4d23d69e026b41e41ed87f0c79c4300
fiahfy <d.fiahfy@gmail.com>
2018-07-13 09:22:28 +0900

    Remove unused code

--- src/background.js
+++ src/background.js
@@ -12,66 +12,10 @@ const code = `
 `
 
 const enabled = {}
-// const urls = {}
-// const redirected = {}
 
-// const getIndex = (url) => {
-//   let matches = url.match(/index=(\d+)/)
-//   if (!matches) {
-//     return 0
-//   }
-//   let [, index] = matches
-//   return Number(index)
-// }
-
-// chrome.webRequest.onBeforeRequest.addListener((details) => {
-//   Logger.log('chrome.webRequest.onBeforeRequest', details)
-
-//   const { tabId, url } = details
-//   Logger.log(enabled)
-//   if (!enabled[tabId]) {
-//     return
-//   }
-//   if (redirected[tabId]) {
-//     return
-//   }
-
-//   const currentIndex = getIndex(urls[tabId])
-//   const index = getIndex(url)
-
-//   let newIndex = 0
-//   if (index === currentIndex - 1) {
-//     newIndex = currentIndex + 1
-//   } else if (index === currentIndex + 1) {
-//     newIndex = currentIndex - 1
-//   } else if (currentIndex === 1) {
-//     Logger.log('cancel')
-//     return { cancel: true }
-//   }
-//   // return { cancel: true }
-
-//   Logger.log(newIndex)
-
-//   if (!newIndex) {
-//     return
-//   }
-
-//   redirected[tabId] = true
-
-//   let redirectUrl = url.replace(/(index=)(\d+)/, '$1' + newIndex)
-//   Logger.log(redirectUrl)
-//   redirectUrl = redirectUrl.replace(/[?&]pbj=1/, '')
-//   Logger.log(redirectUrl)
-
-//   // chrome.tabs.sendMessage(tabId, { id: 'urlChanged', data: { url: 'https://www.youtube.com/watch?v=8ThWNLpvgho&list=PL7LdRPp7xCkM3ZWVsViz2vHTx0YQPiN10&index=7' } })
-
-//   // return
-//   return { cancel: true }
-
-//   // return {
-//   //   redirectUrl: 'https://www.google.com/#q=' + q
-//   // }
-// }, { urls: ['https://www.youtube.com/watch?v=*&index=*'] }, ['blocking'])
+const sendMessage = (tabId) => {
+  chrome.tabs.sendMessage(tabId, { id: 'stateChanged', data: { enabled: enabled[tabId] } })
+}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   Logger.log('chrome.runtime.onMessage', message, sender, sendResponse)
@@ -80,22 +24,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   const { tab } = sender
   switch (id) {
     case 'contentLoaded':
-      // urls[tab.id] = data.url
-      // redirected[tab.id] = false
-      // enabled[tab.id] = false
       chrome.tabs.insertCSS(tab.id, { code })
-      chrome.tabs.sendMessage(tab.id, { id: 'stateChanged', data: { enabled: enabled[tab.id] } })
+      sendMessage(tab.id)
       break
     case 'stateChanged':
-      // urls[tab.id] = data.url
-      // redirected[tab.id] = false
       enabled[tab.id] = !enabled[tab.id]
-      chrome.tabs.sendMessage(tab.id, { id: 'stateChanged', data: { enabled: enabled[tab.id] } })
+      sendMessage(tab.id)
       break
     case 'buttonRemoved':
-      // urls[tab.id] = data.url
-      // redirected[tab.id] = false
-      chrome.tabs.sendMessage(tab.id, { id: 'stateChanged', data: { enabled: enabled[tab.id] } })
+      sendMessage(tab.id)
       break
   }
 })
--- src/content-scripts.js
+++ src/content-scripts.js
@@ -20,14 +20,6 @@ const querySelectorAsync = async (selector) => {
   })
 }
 
-// const wait = async () => {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve()
-//     }, 1000)
-//   })
-// }
-
 const setupButton = (enabled) => {
   const buttons = document.querySelector('ytd-playlist-panel-renderer ytd-menu-renderer #top-level-buttons')
   const container = document.querySelector('ytd-playlist-panel-renderer iron-list #items')
@@ -100,20 +92,15 @@ const setupVideo = (enabled) => {
   }
 }
 
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-  Logger.log('onMessage', message, sender, sendResponse)
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  Logger.log('chrome.runtime.onMessage', message, sender, sendResponse)
 
   const { id, data } = message
   switch (id) {
     case 'stateChanged':
-      await setupButton(data.enabled)
-      await sortList(data.enabled)
-      await setupVideo(data.enabled)
-      break
-    case 'urlChanged':
-      // setTimeout(() => {
-      // location.href = url
-    // }, 1000)
+      setupButton(data.enabled)
+      sortList(data.enabled)
+      setupVideo(data.enabled)
       break
   }
 })
@@ -124,49 +111,11 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   const observer = new MutationObserver(async (mutations) => {
     const [mutation] = mutations
     if (mutation.removedNodes.length) {
-      chrome.runtime.sendMessage({ id: 'buttonRemoved', data: { url: location.href } })
+      chrome.runtime.sendMessage({ id: 'buttonRemoved' })
     }
   })
   const item = await querySelectorAsync('#top-level-buttons')
   observer.observe(item, { childList: true })
 
-  // const video = document.querySelector('video')
-  // Logger.log(getEventListeners(video))
-  // getEventListeners(video).ended.forEach((e) => { e.remove() })
-
-  chrome.runtime.sendMessage({ id: 'contentLoaded', data: { url: location.href } })
-
-  // return
-
-  // const items = Array.from(container.querySelectorAll('ytd-playlist-panel-video-renderer'))
-  // Logger.log(items)
-  // Logger.log(items.reverse())
-  // // .forEach(async (item) => {
-  // //   Logger.log(item)
-  // //   container.append(item)
-  // //   await wait()
-  // // })
-  // for (let item of items) {
-  //   // item.remove()
-  //   // document.body.append(item)
-  //   container.append(item)
-  //   Logger.log(item)
-  //   Logger.log(1)
-  //   // await wait()
-  // }
-  // await wait()
-  // for (let item of items) {
-  //   // item.remove()
-  //   // document.body.append(item)
-  //   // container.append(item)
-  //   // Logger.log(item)
-  //   // Logger.log(1)
-  //   // await wait()
-  // }
-  // buttons.append = buttons.innerHTML + `
-  // <ytd-toggle-button-renderer button-renderer="" class="style-scope ytd-menu-renderer style-grey-text" is-icon-button="" has-no-text=""><a class="yt-simple-endpoint style-scope ytd-toggle-button-renderer" tabindex="-1"><yt-icon-button id="button" class="style-scope ytd-toggle-button-renderer style-grey-text" aria-pressed="false"><button id="button" class="style-scope yt-icon-button" aria-label="再生リストをシャッフル"><yt-icon class="style-scope ytd-toggle-button-renderer"><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon">
-  //       <path d="M10.59 9.17L5.41 4 4 5.41l5.17 5.17 1.42-1.41zM14.5 4l2.04 2.04L4 18.59 5.41 20 17.96 7.46 20 9.5V4h-5.5zm.33 9.41l-1.41 1.41 3.13 3.13L14.5 20H20v-5.5l-2.04 2.04-3.13-3.13z" class="style-scope yt-icon"></path>
-  //     </g></svg>
-  // </yt-icon></button></yt-icon-button></a></ytd-toggle-button-renderer>
-  // `
+  chrome.runtime.sendMessage({ id: 'contentLoaded' })
 })()
